9fbad97e43d755ddc1b1d1f1df8cd119d051a49c
Leonardo <leonardoleoreissouza05@gmail.com>
2024-10-11 22:43:47 -0300

    feat: comecei

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User; 
+
+class WelcomeController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        
+        return view('welcome');
+    }
+
+}
